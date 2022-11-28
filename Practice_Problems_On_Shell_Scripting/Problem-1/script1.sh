4a468cdcc2c2db3ef6aec3fe05b0822d6081e42e
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2022-11-28 08:51:43 +0600

    Practice Problem 1 solved

---
+++ Practice_Problems_On_Shell_Scripting/Problem-1/script1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+(( $# < 1 )) && echo "Usage script1.sh filename1 filename2 ..." && exit 1
+
+for file in "$@"
+do
+	if [[ ! -e $file ]]
+	then 
+		echo "$file doesn't exist"
+	elif [[ -f $file ]]
+	then 
+		if [[ -x $file ]]
+		then 
+			echo "$file is currently executable"
+			echo $(ls -all "$file")
+			echo "$file's executable permission is now changing"
+			$(chmod -x "$file") && echo $(ls -all "$file") && echo "$file is currently not executable"
+		else
+			echo "$file is currently not executable" && echo $(ls -all "$file")
+		fi
+			
+	else
+		echo "$file is not a regular file"
+	fi
+done
