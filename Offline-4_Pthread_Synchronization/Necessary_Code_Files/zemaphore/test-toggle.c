0f6935585b935427ed61656fba65f439b25cb0da
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2023-02-06 16:53:46 +0600

    Zemaphore problem solved

--- Offline-4_Pthread_Synchronization/Necessary_Code_Files/zemaphore/test-toggle.c
+++ Offline-4_Pthread_Synchronization/Necessary_Code_Files/zemaphore/test-toggle.c
@@ -11,6 +11,7 @@
 #define NUM_ITER 10
 
 zem_t zemaphore_t[NUM_THREADS];
+int turn = 0;
 
 void *justprint(void *data)
 {
@@ -19,7 +20,8 @@ void *justprint(void *data)
     for(int i=0; i < NUM_ITER; i++) {
         zem_down(&zemaphore_t[thread_id]);
         printf("This is thread %d\n", thread_id);
-        zem_up(&zemaphore_t[(thread_id+1)%NUM_THREADS]);
+        turn = (turn + 1) % NUM_THREADS;
+        zem_up(&zemaphore_t[turn]);
     }
     return 0;
 }
@@ -30,10 +32,14 @@ int main(int argc, char *argv[])
     pthread_t mythreads[NUM_THREADS];
     int mythread_id[NUM_THREADS];
 
-    
+    // Start with thread 0 ->
+    zem_init(&zemaphore_t[0], 1);
+    for (int i = 1; i < NUM_THREADS; i++) {
+        zem_init(&zemaphore_t[i], 0);
+    }
+
     for(int i =0; i < NUM_THREADS; i++) {
         mythread_id[i] = i;
-        zem_init(&zemaphore_t[i], 0);
         pthread_create(&mythreads[i], NULL, justprint, (void *)&mythread_id[i]);
     }
     
